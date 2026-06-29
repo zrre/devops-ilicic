8d7df95b759e6fdc7a3cc500e3e521e144759f98
Zarko Nebrigic <zarko.nebrigic@prodyna.com>
2026-06-30 00:20:15 +0200

    Format OpenTofu and Terragrunt files

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -18,11 +18,11 @@ resource "azurerm_network_security_group" "this" {
 resource "azurerm_subnet" "this" {
   for_each = var.subnets
 
-  name                 = each.value.name
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.this.name
-  address_prefixes     = each.value.address_prefixes
-  service_endpoints    = each.value.service_endpoints
+  name                              = each.value.name
+  resource_group_name               = var.resource_group_name
+  virtual_network_name              = azurerm_virtual_network.this.name
+  address_prefixes                  = each.value.address_prefixes
+  service_endpoints                 = each.value.service_endpoints
   private_endpoint_network_policies = each.value.private_endpoint_network_policies
 }
 
